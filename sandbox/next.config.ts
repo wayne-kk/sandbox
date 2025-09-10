ad2fac3d98d6974b6a93e67a61966cccfb71b162
wayne-kk <wangwei@aigcode.net>
2025-09-10 14:08:36 +0800

    feat: v0 sandbox

--- sandbox/next.config.ts
+++ sandbox/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: '',
+  basePath: '/sandbox',
+  assetPrefix: '/sandbox',
   images: {
     remotePatterns: [
       {
